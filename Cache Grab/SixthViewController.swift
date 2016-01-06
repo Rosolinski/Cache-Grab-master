ccb1a7304ad2b37d97a031c739872bf240a872e8
Rosolinski <rosolinski@googlemail.com>
2016-01-06 03:12:45 +0000

    Location and info tweaks, added ViewController

---
+++ Cache Grab/SixthViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FourthViewController.swift
+//  Cache Grab
+//
+//  Created by Robin Osolinski on 22/12/2015.
+//  Copyright © 2015 Robin Osolinski. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Foundation
+
+class SixthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
